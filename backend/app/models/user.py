63d0a543eaa22c6374294bb83ac7217bba0b8c86
Bruce Olivier <bolivierjr@gmail.com>
2018-06-10 15:20:56 -0500

    Added some error hanlding and returned some 500 http errors.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,13 +1,14 @@
 from backend.db import db, ma
 from werkzeug.security import generate_password_hash
+from sqlalchemy.exc import OperationalError
 
 
 class UserModel(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(128))
-    email = db.Column(db.String(80), unique=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
 
     def __init__(self, username, password, email):
         self.username = username
@@ -15,8 +16,12 @@ class UserModel(db.Model):
         self.email = email
 
     def save(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+
+        except OperationalError:
+            raise
 
     def delete(self):
         db.session.add(self)
--- backend/app/resources/user.py
+++ backend/app/resources/user.py
@@ -1,8 +1,9 @@
 from backend.app.models.user import UserModel, UserSchema
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from flask_jwt_extended import create_access_token
 from flask import request
 from werkzeug.security import check_password_hash
+from sqlalchemy.exc import DBAPIError, OperationalError
 
 
 class UserRegister(Resource):
@@ -15,49 +16,71 @@ class UserRegister(Resource):
         Return proper JSON response back from the API given the parameters.
         """
 
-        json = request.get_json()
-        username = str(json.get('username'))
-        password = str(json.get('password'))
-        email = str(json.get('email'))
+        try:
+            json = request.get_json()
+            username = str(json.get('username'))
+            password = str(json.get('password'))
+            email = str(json.get('email'))
 
-        new_user = {
-            'username': username,
-            'password': password,
-            'email': email
-        }
+            new_user = {
+                'username': username,
+                'password': password,
+                'email': email
+            }
 
-        if not username or not password or not email:
-            return {'message': 'Fields must not be blank'}, 400
+            if not username or not password or not email:
+                return {'message': 'Fields must not be blank'}, 400
 
-        elif UserModel.find_by_username(username):
-            return {'message': 'Username already exists'}, 400
+            elif UserModel.find_by_username(username):
+                return {'message': 'Username already exists'}, 400
 
-        elif UserModel.find_by_email(email):
-            return {'message': 'Email already exists'}, 400
+            elif UserModel.find_by_email(email):
+                return {'message': 'Email already exists'}, 400
 
-        user = cls.user_schema.load(**new_user).data
-        user.save()
+            user = cls.user_schema.load(**new_user).data
+            user.save()
 
-        return {'message': 'User created successfully'}, 201
+            return {'message': 'User created successfully'}, 201
+
+        except OperationalError or DBAPIError as e:
+            print(e.args)
+
+            return {'message': 'Database error. Please contact an admin.'}, 500
+
+        except Exception as e:
+            print(e.args)
+
+            return {'message': 'Error with server. Please contact an admin.'}
 
 
 class UserLogin(Resource):
     def post(self):
-        json = request.get_json()
+        try:
+            json = request.get_json()
 
-        user = UserModel.find_by_username(str(json.get('username')))
+            user = UserModel.find_by_username(str(json.get('username')))
 
-        if user:
-            password = check_password_hash(
-                user.password, str(json.get('password')))
+            if user:
+                password = check_password_hash(
+                    user.password, str(json.get('password')))
 
-            if not password:
-                return {'message': 'Invalid Password'}
+                if not password:
+                    return {'message': 'Invalid Password'}
 
-        else:
-            return {'message': 'Invalid Username'}, 401
+            else:
+                return {'message': 'Invalid Username'}, 401
 
-        if user and password:
-            access_token = create_access_token(identity=user.id, fresh=True)
+            if user and password:
+                access_token = create_access_token(identity=user.id, fresh=True)
 
-            return {'access_token': access_token}, 200
+                return {'access_token': access_token}, 200
+
+        except OperationalError or DBAPIError as e:
+            print(e.args)
+
+            return {'message': 'Database error. Please contact an admin.'}, 500
+
+        except Exception as e:
+            print(e.args)
+
+            return {'message': 'Error with server. Please contact ad admin.'}, 500
