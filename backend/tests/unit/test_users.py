fa4affd966c9b910b23dde1d2128736c11678eb7
Bruce Olivier <bolivierjr@gmail.com>
2019-07-16 08:07:50 -0500

    Refactored and added some more unit tests for the user model to make it 100% coverage in that section.

--- backend/tests/unit/test_users.py
+++ backend/tests/unit/test_users.py
@@ -2,10 +2,46 @@ from backend.api.models import User
 from werkzeug.security import check_password_hash
 
 
-def test_new_user():
-    user = User("test", "password", "test@gmail.com")
-
+# Assertion helper function to test the user model
+def assert_user(user):
+    assert user is not None
     assert user.email == "test@gmail.com"
     assert user.password != "password"
     assert check_password_hash(user.password, "password")
     assert user.username == "test"
+    assert user.id == 2
+    assert repr(user) == "<User test>"
+
+
+def test_save_user():
+    user = User("test", "password", "test@gmail.com")
+    user.save()
+
+    assert_user(user)
+
+
+def test_find_by_email():
+    user = User.find_by_email("test@gmail.com")
+
+    assert_user(user)
+
+
+def test_find_by_username():
+    user = User.find_by_username("test")
+
+    assert_user(user)
+
+
+def test_find_user_by_id():
+    user = User.find_by_id(2)
+
+    assert_user(user)
+
+
+def test_delete_user():
+    user = User.find_by_username("test")
+    user.delete()
+
+    user = User.find_by_username("test")
+
+    assert user is None
